ecff91168348573025d1037098ba0c5e0840d61a
andresogc <andresogc2011@gmail.com>
2022-03-25 14:53:59 -0500

    Se crea el middleware para manejo de errores

--- api/index.js
+++ api/index.js
@@ -6,6 +6,8 @@ const config = require('../config.js');
 const user = require('./components/user/network');
 const auth = require('./components/auth/network');
 
+const errors = require('../network/errors'); 
+
 
 const app = express();
 
@@ -15,6 +17,7 @@ app.use(bodyParser.json());
 app.use('/api/user', user);
 app.use('/api/auth', auth);
 
+app.use(errors);   //middleware de errors debe ir siempre de ultimas sino no actuaria con los componentes que esten despues
 
 app.listen(config.api.port,()=>{
     console.log('Api escuchando en el puerto', config.api.port);
--- auth/index.js
+++ auth/index.js
@@ -1,22 +1,23 @@
 const jwt = require('jsonwebtoken');
 const config = require('../config');
-const secreto = config.jwt.secret;
+const error = require('../utils/error');
+
+const secret = config.jwt.secret;
 
 function sign(data){
-    return jwt.sign(data,secreto);
+    return jwt.sign(data,secret);
 }
 
 function verify(token) {
-    return jwt.verify(token,secreto)
+    return jwt.verify(token,secret)
 }
 
 const check = {
     own: function(req,owner){ 
         const decoded = decodeHeader(req);
-        console.log('decoded',owner);
-        //comprobar si es o no propio
+        
         if(decoded.id !== owner){
-            throw new Error('No tienes permiso para esta acción');
+            throw error('No tienes permiso para esta acción',401);
         }
     },
 
@@ -24,11 +25,11 @@ const check = {
 
 function getToken(auth) {
     if(!auth){
-        throw new Error('No viene token');
+        throw error('No viene token',401);
     }
 
     if (auth.indexOf('Bearer ') === -1)  {
-        throw new Error('Formato invalido');
+        throw error('Formato invalido',401);
     }
 
     let token = auth.replace('Bearer ','');
